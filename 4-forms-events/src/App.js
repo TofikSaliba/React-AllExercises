78028728733ec4297c9908579de1cf93c5ee9c65
TofikSaliba <tofik.saliba@gmail.com>
2022-05-17 22:30:40 +0300

    added folder 4-forms

---
+++ 4-forms-events/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+// import EXERCISE4_1 from "./Components/Exercise4_1/CustomButton";
+// import EXERCISE4_2 from "./Components/Exercise4_2/ImgCard";
+// import EXERCISE6_1 from "./Components/Exercise6_1/Exercise6_1";
+import EXERCISE7_1 from "./Components/Exercise7_1/Exercise7_1";
+// import EXERCISE7_2 from "./Components/Exercise7_2/Exercise7_2";
+// import EXERCISE7_3 from "./Components/Exercise7_3/Exercise7_3";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {/* <EXERCISE4_1 /> */}
+        {/* <EXERCISE4_2 /> */}
+        {/* <EXERCISE6_1 /> */}
+        <EXERCISE7_1 />
+        {/* <EXERCISE7_2 /> */}
+        {/* <EXERCISE7_3 /> */}
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ 4-forms-events/src/Components/Exercise7_1/Exercise7_1.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { time: new Date().toLocaleTimeString() };
+  }
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        time: new Date().toLocaleTimeString(),
+      });
+      console.log("hi");
+    }, 1000);
+  }
+
+  render() {
+    return <div className="time">The time is: {this.state.time}</div>;
+  }
+}
+
+export default Clock;

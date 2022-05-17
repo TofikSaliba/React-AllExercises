5617c632e328d43ebae69faaea126a4e1648eace
TofikSaliba <tofik.saliba@gmail.com>
2022-05-17 15:56:16 +0300

    added ex 8.2

--- 3-cycle-methods/src/App.js
+++ 3-cycle-methods/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
-import EXERCISE8_1 from "./Components/Exercise8_1/FavoriteColor";
+// import EXERCISE8_1 from "./Components/Exercise8_1/FavoriteColor";
+import EXERCISE8_2 from "./Components/Exercise8_2/BoxAnimation";
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <EXERCISE8_1 />
+        {/* <EXERCISE8_1 /> */}
+        <EXERCISE8_2 className="mediumBox" />
+        <EXERCISE8_2 className="largeBox" />
+        <EXERCISE8_2 className="smallBox" />
       </div>
     );
   }

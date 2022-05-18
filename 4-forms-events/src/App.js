af1e73230bb8dcbd89bec3a67f7cf5ae3b311d73
TofikSaliba <tofik.saliba@gmail.com>
2022-05-18 12:00:33 +0300

    added ex 11.1

--- 4-forms-events/src/App.js
+++ 4-forms-events/src/App.js
@@ -1,21 +1,30 @@
 import React from "react";
-// import EXERCISE4_1 from "./Components/Exercise4_1/CustomButton";
-// import EXERCISE4_2 from "./Components/Exercise4_2/ImgCard";
-// import EXERCISE6_1 from "./Components/Exercise6_1/Exercise6_1";
-import EXERCISE7_1 from "./Components/Exercise7_1/Exercise7_1";
-// import EXERCISE7_2 from "./Components/Exercise7_2/Exercise7_2";
-// import EXERCISE7_3 from "./Components/Exercise7_3/Exercise7_3";
+import EXERCISE11_1 from "./Components/Exercise11_1/ColoredBtn";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.colors = ["blue", "red", "yellow"];
+    this.state = { selected: "" };
+  }
+
+  onChildBtnClick = (color) => {
+    this.setState({ selected: color });
+  };
+
   render() {
     return (
       <div>
-        {/* <EXERCISE4_1 /> */}
-        {/* <EXERCISE4_2 /> */}
-        {/* <EXERCISE6_1 /> */}
-        <EXERCISE7_1 />
-        {/* <EXERCISE7_2 /> */}
-        {/* <EXERCISE7_3 /> */}
+        <div className="btnsContainer">
+          {this.colors.map((clr) => (
+            <EXERCISE11_1
+              key={clr}
+              color={clr}
+              onClick={this.onChildBtnClick}
+            />
+          ))}
+        </div>
+        <h1 id="msg">{`The color selected is: ${this.state.selected}`}</h1>
       </div>
     );
   }

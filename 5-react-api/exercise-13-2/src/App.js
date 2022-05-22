1753c3e5c573349fc394a26b4917d1423e5ca153
TofikSaliba <tofik.saliba@gmail.com>
2022-05-22 18:25:55 +0300

    added ex 13.2

---
+++ 5-react-api/exercise-13-2/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Avatars from "./Components/Avatars";
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <Avatars />
+      </div>
+    );
+  }
+}
+
+export default App;
